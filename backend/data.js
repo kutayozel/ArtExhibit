853cca7d63b3ba6187ab68ee03c1b1255522a9cd
Kharza <zelkutay@gmail.com>
2021-08-31 10:37:20 +0300

    mongoose

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs'
+
 const data = {
+    users: [
+        {
+            name: 'Kutay',
+            email: 'admin@example.com',
+            password: bcrypt.hashSync('1234', 8),
+            isAdmin: true,
+        },
+        {
+            name: 'User',
+            email: 'user@example.com',
+            password: bcrypt.hashSync('1234', 8),
+            isAdmin: false,
+        },
+    ],
     products:[
         {
             id:'1',
